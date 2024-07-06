7a1b591ce254a35085eae51ba74fa6dab9f8da09
Nazadain <oldfather3@mail.ru>
2024-07-06 19:45:53 +0500

    API(need fixes and improvements)

--- server/src/routes/ingredientLink.router.js
+++ server/src/routes/ingredientLink.router.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 const ingredientLinkController = require("../controllers/ingredientLink.controller");
+const { checkAuth } = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+const checkRoleMiddleware = require("../middlewares/checkRole.middleware");
 
-router.post("/", ingredientLinkController.createLink);
+router.post("/", authMiddleware, ingredientLinkController.createLink);
 router.get("/", ingredientLinkController.getLinks);
 router.get("/:id", ingredientLinkController.getLinksByTableItemId);
-router.delete("/:id", ingredientLinkController.deleteLink);
+router.delete(
+  "/:id",
+  checkAuth,
+  checkRoleMiddleware("ADMIN", "MANAGER"),
+  ingredientLinkController.deleteLink
+);
 
 module.exports = router;
